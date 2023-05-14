1d499b932668991ef2f3543ef91d0b92c166c035
v4zha <arvin_j4848@protonmail.com>
2023-05-14 22:47:05 +0530

    sp-ui-update

--- sp_recog.py
+++ sp_recog.py
@@ -1,33 +1,31 @@
+import sys
 import cv2
 import numpy as np
-import tkinter as tk
 import speech_recognition as sr
 from sp_proc import SpProc
-from PIL import Image, ImageTk
+from PyQt5 import QtWidgets, QtGui, QtCore
+import qdarkstyle
 
 
-class SpRecog:
-    def __init__(self, window, title):
-        self.window = window
-        self.window.title(title)
+class SpRecog(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(SpRecog, self).__init__(parent)
+        self.setWindowTitle("Speech-Recog")
         self.sp = SpProc()
         self.res_text = ""
         frame = np.zeros((self.sp.height, self.sp.width, 3), dtype=np.uint8)
-        image = Image.fromarray(frame)
-        self.image = ImageTk.PhotoImage(image)
+        self.image = QtGui.QImage(
+            frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
         self.seq = None
-        self.canvas = tk.Canvas(self.window, width=self.sp.width,
-                                height=self.sp.height)
-        self.canvas.pack()
-        self.canvas.create_image(0, 0, anchor="nw", image=self.image)
-        self.text_label = tk.Label(self.window, text="Res text : ")
-        self.text_label.pack()
-        self.text_box = tk.Text(self.window, height=10)
-        self.text_box.pack()
-        self.rec_start_btn = tk.Button(
-            self.window, text="Record", command=self.rec_audio)
-        self.rec_start_btn.pack()
-        self.window.mainloop()
+
+        self.landing_page = LandingPage(self, self.sp.height, self.sp.width)
+        self.setCentralWidget(self.landing_page)
+
+    def start_app(self):
+        self.landing_page.close()
+        self.recording_page = RecordingPage(
+            self, self.sp.height, self.sp.width)
+        self.setCentralWidget(self.recording_page)
 
     def rec_audio(self):
         r = sr.Recognizer()
@@ -37,34 +35,97 @@ class SpRecog:
             try:
                 self.res_text = r.recognize_google(audio_data=audio)
                 print("Res text : ", self.res_text)
-                self.text_box.delete("1.0", tk.END)
-                self.text_box.insert("1.0", chars=self.res_text)
+                self.recording_page.text_box.setPlainText(self.res_text)
                 gloss = self.sp.get_gloss(self.res_text)
                 self.seq = self.sp.get_seq(gloss, num=None)
-                self.display_seq()
                 if gloss:
                     print("Result gloss : ", gloss)
                 else:
                     print("No matching gloss found")
+                self.timer.start(10)
             except Exception as err:
-                print("Error Recognizing audio : ", err)
+                print(f"Error Recognizing audio : {err}")
 
     def display_seq(self):
         if self.seq:
-            for i, img in enumerate(self.seq):
-                self.image = ImageTk.PhotoImage(image=Image.fromarray(img))
-                self.canvas.create_image(0, 0, anchor="nw", image=self.image)
-                self.window.update()
-                if i == len(self.seq)-1:
-                    break
-                self.window.after(10)
+            img = self.seq.pop(0)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            height, width, channel = img.shape
+            bytesPerLine = 3 * width
+            self.image = QtGui.QImage(
+                img.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
+            self.recording_page.canvas.setPixmap(
+                QtGui.QPixmap.fromImage(self.image))
+            self.recording_page.canvas.setScaledContents(True)
+            QtCore.QTimer.singleShot(10000, self.display_seq)
         else:
             print("No sequence to display")
+            self.timer.stop()
+
+
+class LandingPage(QtWidgets.QWidget):
+    def __init__(self, parent=None, height=480, width=640):
+        super(LandingPage, self).__init__(parent)
+        layout = QtWidgets.QVBoxLayout(self)
+        title_label = QtWidgets.QLabel("Speech Recognition", self)
+        title_font = QtGui.QFont()
+        title_font.setPointSize(24)
+        title_font.setBold(True)
+        title_label.setFont(title_font)
+        layout.addWidget(title_label)
+
+        description_label = QtWidgets.QLabel("Application to show ASL sequences from recognized sentences", self)
+        description_font = QtGui.QFont()
+        description_font.setPointSize(12)
+        description_label.setFont(description_font)
+        layout.addWidget(description_label)
+
+        frame = np.zeros((height, width, 3), dtype=np.uint8)
+        image = QtGui.QImage(
+            frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
+        canvas = QtWidgets.QLabel(self)
+        canvas.setPixmap(QtGui.QPixmap.fromImage(image))
+        layout.addWidget(canvas)
+
+        start_btn = QtWidgets.QPushButton("Start", self)
+        start_btn.clicked.connect(parent.start_app)
+        layout.addWidget(start_btn)
+
+
+class RecordingPage(QtWidgets.QWidget):
+    def __init__(self, parent=None, height=480, width=640):
+        super(RecordingPage, self).__init__(parent)
+        layout = QtWidgets.QVBoxLayout(self)
+
+        self.canvas = QtWidgets.QLabel(self)
+        frame = np.zeros((height, width, 3), dtype=np.uint8)
+        image = QtGui.QImage(
+            frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format_RGB888)
+        self.canvas.setPixmap(QtGui.QPixmap.fromImage(image))
+        layout.addWidget(self.canvas)
+
+        self.text_label = QtWidgets.QLabel("Res text : ", self)
+        layout.addWidget(self.text_label)
+
+        self.text_box = QtWidgets.QTextEdit(self)
+        self.text_box.setFixedHeight(100)
+        layout.addWidget(self.text_box)
+
+        self.rec_start_btn = QtWidgets.QPushButton(self)
+        self.rec_start_btn.setText("Record")
+        self.rec_start_btn.clicked.connect(parent.rec_audio)
+        layout.addWidget(self.rec_start_btn)
+
+        parent.timer = QtCore.QTimer(parent)
+        parent.timer.timeout.connect(parent.display_seq)
 
 
 def main():
-    root = tk.Tk()
-    SpRecog(root, "Speech-Recog")
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    sp_recog = SpRecog()
+    sp_recog.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
